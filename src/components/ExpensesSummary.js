b37c1a8145ea7c95400091701d57da9c764e65e7
xrudoga <defcw58@gmail.com>
2020-02-11 15:16:49 +0800

    Add Expensify styles

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
-import selecttExpensesTotal from '../selectors/expenses-total';
+import selectExpensesTotal from '../selectors/expenses-total';
 
 export const ExpensesSummary = ({ expensesCount, expensesTotal }) => {
     const expensesWord = expensesCount === 1 ? 'expense' : 'expenses';
     const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
     return (
-        <div>
-            <h1>Viewing {expensesCount} {expensesWord} totalling {formattedExpensesTotal}</h1>
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">Viewing <span>{expensesCount}</span> {expensesWord} totalling <span>{formattedExpensesTotal}</span></h1>
+                <div className="page-header__actions">
+                    <Link className="button" to="/create">Add Expense</Link>
+                </div>
+            </div>
         </div>
     )
 };
@@ -18,7 +24,7 @@ const mapStateToProps = (state) => {
     const visibleExpenses = selectExpenses(state.expenses, state.filters);
     return {
         expensesCount: visibleExpenses.length,
-        expensesTotal: selecttExpensesTotal(visibleExpenses)
+        expensesTotal: selectExpensesTotal(visibleExpenses)
     };
 };
 
